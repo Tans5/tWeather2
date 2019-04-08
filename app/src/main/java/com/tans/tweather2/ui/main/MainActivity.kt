273c1ba70b5850ebce18925398c4ca249d0157c0
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2019-04-08 18:27:11 +0800

    Update dependencies.

--- app/src/main/java/com/tans/tweather2/ui/main/MainActivity.kt
+++ app/src/main/java/com/tans/tweather2/ui/main/MainActivity.kt
@@ -9,9 +9,5 @@ class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.fetchWeatherAtmosphere("成都")
-        viewModel.fetchWeatherCondition("成都")
-        viewModel.fetchWeatherForecast("成都")
-        viewModel.fetchWeatherWind("成都")
     }
 }
